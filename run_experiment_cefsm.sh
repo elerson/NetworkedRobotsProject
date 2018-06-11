d97147be8aa377f48892e42573c9ee8cb6488296
Elerson R S Santos <elerss@gmail.com>
2018-06-11 15:50:49 -0300

    running experiments

---
+++ run_experiment_cefsm.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+distancias=(160, 110, 70 )
+diretorios=(1euclideanexperiment 2euclideanexperiment 3euclideanexperiment 4euclideanexperiment 5euclideanexperiment)
+#diretorios=(1euclideanexperiment)
+experiments=(1 2 3 4 5)
+
+for dir_ in ${diretorios[@]}; do
+  export EXP_DIR=~/NetworkedRobotsProject/simulated_experiments/$dir_
+  for dist in ${distancias[@]}; do
+    for exp in ${experiments[@]}; do
+	export LOG_DIR=$dir_$'_gradient_'$dist'/'
+	#export LOG_DIR=$dir_$'_cefsm_'$dist'/'
+	./client/client.py &
+	
+	cd simulated_experiments/gradient/
+	./create_exp.py 20 $dist 0.05
+	cd ../../
+
+	roslaunch simulated_experiments/simulation.launch &
+
+	sleep 5
+
+	roslaunch simulated_experiments/gradient/all.launch
+
+    done
+  done
+done
+
+
+
+
+
+#TODO: 
+#Log
+#numero de robos na solucao
+#Coviriancia
