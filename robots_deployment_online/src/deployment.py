930161ba985d895e7a6178a8f781ba1d998ad298
Elerson R S Santos <elerss@gmail.com>
2018-08-25 15:36:23 -0300

    correcting errors

--- robots_deployment_online/src/deployment.py
+++ robots_deployment_online/src/deployment.py
@@ -919,7 +919,7 @@ class Robot:
         if((dist_to_segmentation > self.high_level_distance or (alpha < 0.1 or alpha > 0.90)) and not self.started_control):
             if(self.status == 1 or self.status == 0):
                 return
-            print('control 1')
+            #print('control 1')
             goal_ = self.getCenterOfPath()
             goal = (goal_[0], goal_[1])
             #goal = (19.695165209372934, 10.23384885215893)
@@ -933,7 +933,7 @@ class Robot:
             if(self.status == 1 or self.status == 0):
                 self.Stall()
             else:
-                print('control 2')
+                #print('control 2')
                 self.control_nonholonomic()
 
        
